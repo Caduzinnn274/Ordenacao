5af7db7c856b19c3b90954736e2d3affbaa7f2e0
Carlos Bressan <105678823+Caduzinnn274@users.noreply.github.com>
2024-09-02 09:50:41 -0300

    Add files via upload

---
+++ ordenacaoNatural.py
@@ -0,0 +1,31 @@
+print("------------------------")
+print("Programa de Ordenação")
+
+print("------------------------")
+print("1 - Ordenação Natural")
+
+#Ordenação natural
+frutas = ["Laranja", "Banana", "Maça", "Mamão"]
+print(frutas)
+
+print("------------------------")
+#Ordenação Customizada, random
+print("2- Ordenação customizada a partir do alfabeto")
+
+
+frutas_sorteadas = ["Laranja", "Banana", "Maça", "Mamão"]
+frutas_sorteadas.sort()
+print(frutas_sorteadas)
+
+print("------------------------")
+#Ordem inversa das frutas
+print("3- Ordem inversa das frutas")
+
+frutas_sorteadas_inverso = ["Laranja", "Banana", "Maça", "Mamão"]
+frutas_sorteadas_inverso.sort(reverse= True)
+print(frutas_sorteadas_inverso) #ordem inversa
+
+
+
+
+
